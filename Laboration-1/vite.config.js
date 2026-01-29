946dd6576ff2389ed12eeba801156f11c7a708f9
vabjo <valabjornsd@gmail.com>
2026-01-29 11:02:20 +0100

    Vite image optimizer error löst: Om bilder är exporterade till avif genom adobe lightroom läser pluginen dem som heif av någon anledning, behöver konverteras genom photoshop (eller liknande) -Valde att använda webp ist för avif, root bilderna har mindre filstorlek som webp (innan optimisering)

--- Laboration-1/vite.config.js
+++ Laboration-1/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import {resolve} from "path";
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
+
 export default defineConfig({
     build: {
         rollupOptions: {
@@ -14,7 +15,7 @@ export default defineConfig({
     },
     plugins: [
         ViteImageOptimizer({
-            avif: {
+            webp: {
                 quality: 60
             }
         })
